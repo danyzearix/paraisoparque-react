6c0927158967f590383032601e6670bcb2ee4150
danydev <atriasoft.ceo@gmail.com>
2023-12-26 18:23:34 -0500

    Usuarios rutas corregidas

--- backend/controllers/usuariosController.js
+++ backend/controllers/usuariosController.js
@@ -40,19 +40,19 @@ const loginUsuarios = async(req, res) => {
         const { email, password } = req.body;
     
         // Buscar al usuario por email
-        const user = await User.findOne({ email });
-        if (!user) {
+        const usuario = await Usuarios.findOne({ email });
+        if (!usuario) {
           return res.status(400).send('Credenciales no válidas');
         }
     
         // Verificar la contraseña
-        const validPassword = await bcrypt.compare(password, user.password);
+        const validPassword = await bcrypt.compare(password, usuario.password);
         if (!validPassword) {
           return res.status(400).send('Credenciales no válidas');
         }
     
         // Crear y enviar el token
-        const token = jwt.sign({ _id: user._id }, 'tu_secreto', { expiresIn: '1h' });
+        const token = jwt.sign({ _id: usuario._id }, 'tu_secreto', { expiresIn: '1h' });
         res.header('auth-token', token).send(token);
       } catch (error) {
         res.status(500).send('Error en el servidor');
