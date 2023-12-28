9504686363a29c1c075ba2ec1a018c7717eee115
Daniel S Munoz <97260392+danyzearix@users.noreply.github.com>
2023-12-27 20:38:11 -0500

    Update uploadRoutes.js

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -3,7 +3,7 @@ import multer from 'multer';
 import uploadFile from '../controllers/uploadController.js'; // Asegúrate de que la ruta sea correcta
 
 const router = express.Router();
-const upload = multer({ dest: 'uploads/' }); // Configura multer según tus necesidades
+const upload = multer({ dest: 'public/uploads/' }); // Configura multer según tus necesidades
 
 router.post('/upload', upload.single('file'), uploadFile);
 
